5667b8ec5e2799415d17177021f6d7c02e19dfd9
kamimore <negivipin996@gmaail.com>
2022-07-16 11:34:36 +0530

    added section for contact and home page

--- resources/views/home/index.blade.php
+++ resources/views/home/index.blade.php
@@ -1,11 +1,7 @@
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
-</head>
-<body>
-    <h1>Hello world!</h1>
-</body>
-</html>
+@extends('layouts.app')
+
+@section('title','Home Page')
+
+@section('content')
+<h1>Hello world!</h1>
+@endsection
