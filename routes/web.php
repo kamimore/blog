ce8beed9b039ae20911947a77d328658f50f2ff4
kamimore <negivipin996@gmaail.com>
2022-07-16 11:23:23 +0530

    added new home contact page

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,7 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('home.index',[]);
 })->name('home.index');
+
+Route::get('/contact',function(){
+    return view('home.contact');
+})->name('home.contact');
