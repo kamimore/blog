adddb1c53e2f1e6618e9c0974f5c8cad75303836
kamimore <negivipin996@gmaail.com>
2022-08-01 22:48:42 +0530

    styling for single blog post

--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -1,11 +1,12 @@
 @extends('layouts.app')
 
-@section('title','Single Post')
+@section('title', $post->title)
 
 @section('content')
-<h3>Single Post Page</h3>
-
-<div>{{ $post->title }}</div>
-<div>{{ $post->content }}
-</div>
+    <h3>{{ $post->title }}</h3>
+    <p>{{ $post->content }}</p>
+    <p>Added {{ $post->created_at->diffForHumans() }}</p>
+    @if (now()->diffInMinutes($post->created_at) < 5)
+        <p class="badge bg-info">New!</p>
+    @endif
 @endsection
