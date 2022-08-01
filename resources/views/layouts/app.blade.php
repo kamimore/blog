0e3e3657bd4c426bc74301f945d29e8ce5515455
kamimore <negivipin996@gmaail.com>
2022-08-01 22:20:59 +0530

    used route() property laravel

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -6,7 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <link rel="stylesheet" href="{{ mix('css/app.css') }}">
-    <script src="{{ mix('js/app.js') }}"></script>
+    <script src="{{ mix('js/app.js') }}" defer></script>
     <title>Laravel App - @yield('title')</title>
 </head>
 
@@ -14,10 +14,10 @@
     <div class="d-flex flex-column flex-md-row align-items-center p-3 px-md-4 bg-white border-bottom shadow-sm mb-3">
         <h3 class="my-0 me-md-auto font-weight-normal">Laravel App</h3>
         <nav class="my-2 my-md-0 me-md-3">
-            <a href="http://blog.com" class="p-2 text-dark text-decoration-none text-wrap">Home</a>
-            <a href="http://blog.com/contact" class="p-2 text-dark text-decoration-none text-wrap" >Contact</a>
-            <a href="http://blog.com/post" class="p-2 text-dark text-decoration-none text-wrap" >Blog Posts</a>
-            <a href="http://blog.com/post/create" class="p-2 text-dark text-decoration-none text-wrap" >Add Blog Post</a>
+            <a href="{{ route('home.index') }}" class="p-2 text-dark text-decoration-none text-wrap">Home</a>
+            <a href="{{ route('home.contact') }}" class="p-2 text-dark text-decoration-none text-wrap" >Contact</a>
+            <a href="{{ route('post.index') }}" class="p-2 text-dark text-decoration-none text-wrap" >Blog Posts</a>
+            <a href="{{ route('post.create') }}" class="p-2 text-dark text-decoration-none text-wrap" >Add Blog Post</a>
         </nav>
     </div>
     <div class="container">
