956b7acf6921887c9a7734615dcfbe69413f912d
kamimore <negivipin996@gmaail.com>
2022-08-02 22:21:13 +0530

    setting up the environment for testign

--- config/database.php
+++ config/database.php
@@ -42,6 +42,12 @@ return [
             'prefix' => '',
             'foreign_key_constraints' => env('DB_FOREIGN_KEYS', true),
         ],
+ 
+        'sqllite_testing' => [
+            'driver' => 'sqlite',
+            'url' => env('DATABASE_URL'),
+            'database' => ':memory',
+        ],
 
         'mysql' => [
             'driver' => 'mysql',
