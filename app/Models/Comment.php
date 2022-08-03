5e6a7259b00795d98a1b0a41ac400ba29dbce4c7
kamimore <negivipin996@gmaail.com>
2022-08-03 12:44:18 +0530

    added comment model

--- app/Models/Comment.php
+++ app/Models/Comment.php
@@ -5,13 +5,12 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class BlogPost extends Model
+class Comment extends Model
 {
     use HasFactory;
-    protected $fillable = ['title','content'];
-
-    public function Comment()
+    
+    public function BlogPost()
     {
-        return $this->hasMany('App\Models\Comment');
+        return $this->belongsTo('App\Models\BlogPost');
     }
 }
